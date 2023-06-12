4cf2ef75a7bb809f9e786bd00f969ae2bc85095d
Mohd Faris P <muhdfarizp2020@gmail.com>
2023-06-12 13:51:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { WiThermometer, WiHumidity } from 'react-icons/wi';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Weather = () => {
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const API_KEY = '38a3bf10fa21eebc76bf3b8f7155ae9a';
+
+  const fetchWeatherData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+      );
+      const data = await response.json();
+
+      if (response.ok) {
+        setWeatherData(data);
+        setError(false);
+      } else {
+        setError(true);
+      }
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="weather-app">
+      <h2 className="title">Weather App</h2>
+      <input
+        type="text"
+        placeholder="Enter a city name"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button onClick={fetchWeatherData}>Get Weather</button>
+      {loading ? (
+        <div className="loader">Loading...</div>
+      ) : error ? (
+        <p>Sorry, data unavailable!</p>
+      ) : weatherData ? (
+        <div className="weather-info">
+      <h2>{weatherData.name}</h2>
+      <p className='temp-text'>
+        <WiThermometer className="weather-icon1" />
+       <span className='temp-data'> Temperature: {weatherData.main.temp} °C </span>
+      </p>
+      <p>
+        <WiHumidity className="weather-icon2" />
+        Humidity: {weatherData.main.humidity}%
+      </p>
+      <p>Description: {weatherData.weather[0].description}</p>
+      <br />
+      <p>
+      <b>
+          Developed by{" "}
+          <a href="https://www.linkedin.com/in/muhammed-faris-pallipath-753259242/?originalSubdomain=in">
+          Trendstube.in
+        </a>
+        </b>
+        </p>
+    </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Weather;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Weather from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Weather />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
